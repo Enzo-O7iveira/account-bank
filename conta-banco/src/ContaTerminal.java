34d27c0d88300b3d88ecf8d4b1a79d8353ddc5f1
Enzo Oliveira <156548563+Enzo-O7iveira@users.noreply.github.com>
2024-03-09 15:05:09 -0300

    ContaTerminal.java

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
-public class ContaTerminal{
-    public static void main(String[] args) throws Exception{
-        System.out.println("Hello World");
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args)  {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite seu CPF ! ");
+            long getCpf = scanner.nextLong();
+
+        System.out.println("Por favor, digite o seu nome !");
+            String getName = scanner.next();
+
+            System.out.println("Por favor, digite o seu sobrenome !");
+            String getFirstName = scanner.next();
+
+        System.out.println("Por favor, digite o número da Agência !");
+            String getAgency = scanner.next();
+        
+        System.out.println("Por favor, digite a quantidade de saque !");
+            double getWithdraw = scanner.nextDouble();
+
+
+
+        System.out.println("Olá " + getName + getFirstName + ", obrigado por criar uma conta em nosso banco, sua agência é " + getAgency + ", criada no CPF: "+ getCpf + " e seu saque de:" + getWithdraw + " já está disponível para saque");
     }
 }
